3787b51c904869938abf1cce5ef6ca668f879b4e
João Vítor de Castro Martins Ferreira Nogueira <joao.nogueira@estudante.ufjf.br>
2021-03-07 19:41:51 -0300

    Adiciona um metodo acabou()

--- js/AssetManager.js
+++ js/AssetManager.js
@@ -8,12 +8,19 @@ export default class AssetManager{
    carregaImagem(chave, source){
 
     const img1 = new Image() ;
+    const that = this;
+    img1.addEventListener("load", ()=> {
+     console.log(`Imagem ${that.carregadas}/${that.aCarregar} carregada!`);
+     that.carregadas++;
+    });
+
     img1.src=source;
     this.imagens.set(chave , img1);
+    this.aCarregar++;
    }
 
    img(chave){
-  return this,imagens.get(chave);
+  return this.imagens.get(chave);
    }
   
    progresso(){
@@ -21,8 +28,10 @@ export default class AssetManager{
     return`${(this.carregadas/this.aCarregar*100).toFixed(2)}%`;
    }
    return "nada a carregar";
+   }
 
-
+   acabou(){
+      return this.carregadas === this.aCarregar;
    }
 
 }
