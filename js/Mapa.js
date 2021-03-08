5108a2ee04aeb50516c3f87fdf1873f10f90b20e
João Vítor de Castro Martins Ferreira Nogueira <joao.nogueira@estudante.ufjf.br>
2021-03-08 00:17:16 -0300

    Adiciona mais um exemplo de tipo de tile

--- js/Mapa.js
+++ js/Mapa.js
@@ -23,6 +23,13 @@ constructor(linhas = 8, colunas = 12, tamanho = 32){
             ctx.strokeStyle = "black";
             ctx.strokeRect(c * this.SIZE, l * this.SIZE, this.SIZE, this.SIZE);
             break;
+            case 2:
+            ctx.fillStyle = "red";
+            ctx.fillRect(c * this.SIZE, l * this.SIZE, this.SIZE, this.SIZE);
+            ctx.linesWidth = 1;
+            ctx.strokeStyle = "orange";
+            ctx.strokeRect(c * this.SIZE, l * this.SIZE, this.SIZE, this.SIZE);
+            break;
             default:
             ctx.fillStyle = "black";
             ctx.fillRect(c * this.SIZE, l * this.SIZE, this.SIZE, this.SIZE);
