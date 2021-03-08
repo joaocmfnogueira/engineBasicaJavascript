e5b7ab5dd8ca01a55caa8aa01b6e30f92e1ce546
João Vítor de Castro Martins Ferreira Nogueira <joao.nogueira@estudante.ufjf.br>
2021-03-08 17:36:20 -0300

    Adiciona restrição de movimento para a direita

--- js/Cena.js
+++ js/Cena.js
@@ -23,6 +23,7 @@ export default class Cena{
     for (let s = 0; s < this.sprites.length; s++) {
         const sprite = this.sprites[s];
         sprite.desenhar(this.ctx);
+        sprite.aplicaRestricoes();
     }
    }
     this.ctx.fillStyle = "yellow";
--- js/Sprite.js
+++ js/Sprite.js
@@ -39,7 +39,25 @@ export default class Sprite {
         this.y + this.h/2 < outro.y - outro.h/2
         );
     }
-
-
-
+    aplicaRestricoes(dt){
+        const SIZE = this.cena.mapa.SIZE;
+      if(this.vx>0){
+        const pmx = this.mx + 1;
+        const pmy = this.my ;
+        if(this.cena.mapa.tiles[pmy][pmx]!=0){
+            const tile = {
+                x: pmx*SIZE+SIZE/2, 
+                y: pmy*SIZE+SIZE/2,
+                w: SIZE,
+                h: SIZE
+        };
+        this.cena.ctx.strokeStyle = "White";
+        this.cena.ctx.strokeRect(tile.x-SIZE/2,tile.y-SIZE/2,SIZE,SIZE);
+        if(this.colidiuCom(tile)){
+            this.vx = 0;
+            this.x = tile.x - tile.w/2 - this.w/2 - 1;
+        }
+      }
+    }
+  }
 }
