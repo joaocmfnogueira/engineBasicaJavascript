4b88fd3293d235a734398870b7b885c523c2dc6a
João Vítor de Castro Martins Ferreira Nogueira <joao.nogueira@estudante.ufjf.br>
2021-03-20 16:10:48 -0300

    Corrige t0 inicial

--- js/Cena.js
+++ js/Cena.js
@@ -7,7 +7,7 @@ export default class Cena{
     this.ctx = canvas.getContext("2d");
     this.sprites = [];
     this.aRemover = [];
-    this.t0 = 0;
+    this.t0 = null;
     this.dt = 0;
     this.idAnim = null;
     this.assets = assets;
